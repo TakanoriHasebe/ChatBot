4615fe88656633f5acfee78c2abc97cebf97564b
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2016-11-26 20:55:46 +0900

    Anonymou button and Google login button is added

---
+++ ChatBot/ChatBot/ChatBot/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  ChatBot
+//
+//  Created by Takanori.H on 2016/11/26.
+//  Copyright © 2016年 Takanori.H. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func LoginAnonymouslyDidTapped(_ sender: AnyObject) {
+        print("login anonymously did tapped")
+    }
+    
+    @IBAction func googleLoginDidTapped(_ sender: AnyObject) {
+        print("google login did tapped")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
